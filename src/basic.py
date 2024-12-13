b81f89c1864f7ff71028328e2699ccf79b8bdefa
Fireond <1824831405@qq.com>
2024-12-13 13:51:59 +0800

    merge

--- src/basic.py
+++ src/basic.py
@@ -73,58 +73,6 @@ class Problem:
                     W += (self.mat_A[i][j] - self.mat_B[f[i]][f[j]]) ** 2
         return W
 
-<<<<<<< HEAD
-# find minimum weight given injection f
-def find_min_weight(N, M, weight_function):
-    if M < N:
-        raise ValueError("M cannot be larger than N!")
-
-    min_weight = float("inf")
-    best_f = None
-
-    # enumerate all permutations for f
-    for perm in itertools.permutations(range(M), N):
-        weight = weight_function(perm)
-        if weight < min_weight:
-            min_weight = weight
-            best_f = perm
-
-    return best_f, min_weight
-
-
-# Evaluate W(f)
-def eval_W(f, A, B, L1, L2):
-    W = 0
-    N, M, L = get_NML(A, B)
-    assert len(f) == N
-    for i in range(N):
-        assert f[i] >= 0 and f[i] < 2**L
-        if f[i] >= M:
-            W += L1
-        for j in range(i, N):
-            if j > i and f[i] == f[j]:
-                W += L2
-            if f[i] < M and f[j] < M:
-                W += (A[i][j] - B[f[i]][f[j]]) ** 2
-    return W
-
-
-# Evaluate d(f), assert f is valid
-def eval_d(f, A, B):
-    d2 = 0
-    N, _, _ = get_NML(A, B)
-    assert valid(f, A, B)
-    for i in range(N):
-        for j in range(i, N):
-            d2 += (A[i][j] - B[f[i]][f[j]]) ** 2
-    d = np.sqrt(d2)
-    return d
-
-
-# Evaluate SIM(f), assert f is valid
-def eval_SIM(f, A, B):
-    return 1 / (1 + eval_d(f, A, B))
-=======
     def eval_d(self, f: list):
         """
         Evaluate d(f), assert f is valid
