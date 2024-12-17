29288837c2e4ee5d1a654ef4693fc3b3523870a6
fireond <yanhy22@mails.tsinghua.edu.cn>
2024-12-17 15:41:26 +0800

    add run_tests.py

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+import json
+from src.qa import QA_simulate
+from src.basic import Problem
+
+if __name__ == "__main__":
+    with open("./tests/test_cases.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    for i, p in enumerate(data):
+        problem = Problem.from_bonds(p['cha_a'], p['cha_b'], p["bonds_a"], p["bonds_b"], p["hydrogen_a"], p["hydrogen_b"])
+        QA_simulate(problem, params={'t0':50, 'm0':100, 'device':'GPU'})
