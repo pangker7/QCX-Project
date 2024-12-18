775afb95126db9e2a6380358d0d661b7aff432ba
Your Name <you@example.com>
2024-12-19 00:43:33 +0800

    plot two cases calculated by qa

--- plot/plot_tsca.py
+++ plot/plot_tsca.py
@@ -12,16 +12,21 @@ def convert_str_to_list_or_tuple(val):
         return val
 
 
-path_tcsa = "../data/TSCA_result"
+path_tcsa = "qa_TSCA_result"
+# path_tcsa = "../data/qva_TSCA_result"
 
-group_idx = 2
-file_name = [f for f in os.listdir(path_tcsa) if f.startswith(str(group_idx + 1))][0]
+group_idx =11
+df_group = pd.read_csv("../data/fun_group.csv")
+group_name_list = df_group["GroupName"].apply(convert_str_to_list_or_tuple)
+file_name = str(group_idx + 1) + f"_{group_name_list[group_idx]}"
+# file_name = [f for f in os.listdir(path_tcsa) if f.startswith(str(group_idx + 1))][0]
 
-df_data = pd.read_csv(f"{path_tcsa}/{file_name}")
+df_data = pd.read_csv(f"../data/{path_tcsa}/{file_name}" + ".csv")
 len_molecule = df_data["len_molecule"].apply(convert_str_to_list_or_tuple)
 valid_prob = df_data["valid_prob"].apply(convert_str_to_list_or_tuple)
 sol_prob = df_data["sol_prob"].apply(convert_str_to_list_or_tuple)
 d_min = df_data["d_min"].apply(convert_str_to_list_or_tuple)
+degeneracy = df_data["degeneracy"].apply(convert_str_to_list_or_tuple)
 num_molecule = len(len_molecule)
 
 has_indices = np.where(d_min == 0)[0]
@@ -42,17 +47,41 @@ def get_sorted_arr(prob, indices):
 
 
 def plot_data(prob, indices, label, color):
-    plt.scatter(len_molecule[indices], prob[indices], label=label, color=color)
+    plt.scatter(len_molecule[indices], prob[indices], label=label, color=color, s=12)
 
 
-def plot_err(prob, indices, color, ecolor):
+def plot_err(prob, indices, color, ecolor, markersize=4.5):
     x, y_list = get_sorted_arr(prob, indices)
     avg = [np.mean(data) for data in y_list]
     err = [np.std(data) for data in y_list]
-    plt.errorbar(x, avg, yerr=err, fmt="o", color=color, ecolor=ecolor, elinewidth=2, capsize=4)
+    plt.errorbar(x, avg, yerr=err, fmt="o", color=color, ecolor=ecolor, elinewidth=1, capsize=3, markersize=markersize)
+    # plt.errorbar(x, avg, fmt="o", color=color, ecolor=ecolor, elinewidth=1, capsize=3, markersize=markersize)
 
 
-def plot_fig(prob, prob_name, indices_list, label_list, color_list, ecolor, plot_err_flag=True, plot_data_flag=True, ylim=(0, 1)):
+def plot_degeneracy_data(prob, indices, label, ylim=(0, 1)):
+    plt.figure()
+    plt.ylim(ylim)
+    color_list = ["#cccceb", "#8d8dd2", "#8080cd", "#4d4d7b", "#333352", "#26263e", "#1a1a29", "#0d0d15", "#000000"]
+    lenth = degeneracy[indices]
+    dict ={}
+    for i in indices:
+        dege = degeneracy[i]
+        if dege not in dict:
+            dict[dege] = [[len_molecule[i]],[prob[i]]]
+        else:
+            dict[dege][0].append(len_molecule[i])
+            dict[dege][1].append(prob[i])
+    for dege, data in dict.items():
+        plt.scatter(data[0], data[1], color=color_list[dege], s=300)
+    plot_err(prob, indices, "red", ecolor="grey", markersize=20)
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
+
+plot_degeneracy_data(sol_prob, has_indices, "has")
+
+def plot_total_fig(prob, prob_name, indices_list, label_list, color_list, ecolor, plot_err_flag=True, plot_data_flag=True, ylim=(0, 1)):
     """
     indices_list = [has, not_has], label_list = [has, not_has], color_list = [has, not_has, avg]
     """
@@ -71,11 +100,28 @@ def plot_fig(prob, prob_name, indices_list, label_list, color_list, ecolor, plot
     plt.savefig(f"{fig_name}.pdf")
     plt.close()
 
+def plot_sub_fig(prob, prob_name, indices, label, color_list, ecolor, ylim=(0, 1)):
+    plt.figure()
+    plt.ylim(ylim)
+    i = 0 if label == "has" else 1
+    plot_data(prob, indices, label, color_list[i])
+    plot_err(prob, indices, color_list[2], ecolor)
+    # plt.grid()
+    plt.legend()
+    plt.savefig(f"../figure/{path_tcsa}/{file_name}_{label}_{prob_name}.pdf")
+    plt.close()
+
+
 
 indices_list = [has_indices, not_has_indices]
 label_list = ["has", "not_has"]
 # [data, err]
 # color_list = [["#1f77b4", "#aec7e8"], ["#ff7f0e", "#ffbb78"]]
-color_list = ["#1f77b4", "#ff7f0e", "red"]
+color_list = ["#1f70a9", "#1f70a9", "#c22f2f"]
 
-plot_fig(valid_prob, "valid", indices_list=indices_list, label_list=label_list, color_list=color_list, ecolor="grey", ylim=(0.7, 1))
+# plot_total_fig(sol_prob, "sol", indices_list=indices_list, label_list=label_list, color_list=color_list, ecolor="grey", ylim=(0, 1))
+# plot_total_fig(valid_prob, "valid", indices_list=indices_list, label_list=label_list, color_list=color_list, ecolor="grey", ylim=(0.7, 1))
+plot_sub_fig(valid_prob, "valid", has_indices, "has", color_list, ecolor="grey", ylim=(0.6, 1))
+plot_sub_fig(valid_prob, "valid", not_has_indices, "not_has", color_list, ecolor="grey", ylim=(0.6, 1))
+plot_sub_fig(sol_prob, "sol", has_indices, "has", color_list, ecolor="grey")
+plot_sub_fig(sol_prob, "sol", not_has_indices, "not_has", color_list, ecolor="grey")
